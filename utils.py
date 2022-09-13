3334d24086f69b18e2a6cf2ba69a2ae556cf9dc2
Jani Hietala <jani.hietala@vtt.fi>
2022-09-13 18:21:44 +0300

    Update utils

--- utils.py
+++ utils.py
@@ -1,28 +1,28 @@
 import uuid
-from datetime import datetime
-from typing import List, Tuple, Any, Dict
-
-import pytz
+from datetime import datetime, timezone
+from typing import Any, NoReturn
 
 
 def utc_rfc3339_to_datetime(date: str) -> datetime:
-    """Convert UTC RFC 3339 datetime string to Python datetime object"""
+    """Convert UTC RFC 3339 datetime string to datetime object"""
+    return datetime.fromisoformat(date.replace("Z", "")).replace(tzinfo=timezone.utc)
 
-    return datetime.fromisoformat(date.replace("Z", "")).replace(tzinfo=pytz.UTC)
+
+def unix_to_datetime(timestamp: int) -> datetime:
+    """Convert UTC unix timestamp (microseconds) to datetime object"""
+    return datetime.utcfromtimestamp(timestamp / 1e6)
 
 
-def date_string_to_unix(date: str) -> int:
-    """Convert UTC datetime string to unix timestamp"""
-
-    date_object = datetime.fromisoformat(date.replace("Z", "")).replace(tzinfo=pytz.UTC)
-    # Convert to microseconds
+def utc_rfc3339_to_unix(date: str) -> int:
+    """Convert UTC datetime string to unix timestamp in microseconds"""
+    date_object = utc_rfc3339_to_datetime(date)
+    # Convert to unix timestamp in microseconds
     return int(date_object.timestamp() * 1e6)
 
 
-def unix_to_date_string(timestamp: int) -> str:
+def unix_to_utc_rfc3339(timestamp: int) -> str:
     """Convert UTC unix timestamp to datetime string"""
-
-    date_object = datetime.utcfromtimestamp(timestamp / 1e6)
+    date_object = unix_to_datetime(timestamp)
     # Convert to RFC3339 while removing trailing zeroes
     return (
         date_object.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
@@ -32,21 +32,33 @@ def unix_to_date_string(timestamp: int) -> str:
 
 
 def new_uuid() -> str:
+    """Return a new uuid4 as string"""
     return str(uuid.uuid4())
 
 
-def exclude_none_values(dct: List[Tuple[str, Any]]) -> Dict[str, Any]:
-    """Drop 'None' values from given dict"""
+def exclude_none_values(dct: list[tuple[str, Any]]) -> dict[str, Any]:
+    """Drop keys with 'None' value from given dict"""
     return {key: value for (key, value) in dct if value is not None}
 
 
+def require_attribute(object: Any, attribute_name: str) -> Any:
+    """Return attribute or raise AttributeError"""
+    attribute = getattr(object, attribute_name)
+    if not attribute:
+        raise AttributeError(
+            f"'{type(object).__name__}' missing required attribute '{attribute_name}'."
+        )
+    return attribute
+
+
 if __name__ == "__main__":
-    # now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-    now = utc_rfc3339_to_datetime("2022-06-09T14:38:31.945Z")
-    print(now)
-    ux = date_string_to_unix("2022-06-09T14:38:31.945Z")
-    print(ux)
-    now = unix_to_date_string(ux)
-    print(now)
-    dt = utc_rfc3339_to_datetime(now)
-    print(dt)
+    rfc3339_string = "2022-06-09T14:38:31.945Z"
+    print("RFC3339:", rfc3339_string)
+    datetime_object = utc_rfc3339_to_datetime(rfc3339_string)
+    print("Datetime:", datetime_object)
+    unix_timestamp = utc_rfc3339_to_unix(rfc3339_string)
+    print("Unix:", unix_timestamp)
+    rfc3339_string = unix_to_utc_rfc3339(unix_timestamp)
+    print("RFC3339:", rfc3339_string)
+    datetime_object = utc_rfc3339_to_datetime(rfc3339_string)
+    print("Datetime:", datetime_object)
