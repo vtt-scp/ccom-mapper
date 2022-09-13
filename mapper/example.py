d60a667e97bac9fc7111c31e02e751a6a1319134
Jani Hietala <jani.hietala@vtt.fi>
2022-09-13 18:24:43 +0300

    Update examples to cover additional codecs

--- mapper/example.py
+++ mapper/example.py
@@ -5,13 +5,13 @@ from mapper import encoder
 
 # ccom_object = json.load(ccom_json, object_hook=decoder.decode_ccom)
 
-with open("./ccom/tests/test_ccom_measurements.json") as ccom_file:
+with open("./mapper/tests/test_ccom_measurements.json") as ccom_file:
     ccom_dict = json.load(ccom_file)
-ccom_object = decoder.simple_decode_ccom(ccom_dict)
-print(ccom_object)
+ccom_object = decoder.ccom(ccom_dict)
+# print(ccom_object)
 
-ccom_json = encoder.encode_ccom(ccom_object)
-print(ccom_json)
+ccom_json = json.dumps(encoder.ccom(ccom_object), indent=4)
+# print(ccom_json)
 # partner_json = encoder.encode_iot(ccom_object)
 #
 # ccom_json = encoder.encode(encoder.Encoder.CCOM, ccom_object)
@@ -37,7 +37,11 @@ iot_message = {
     ]
 }
 
-ccom_object = decoder.iot_ticket(iot_message)
-print(ccom_object)
-ccom_json = encoder.encode_ccom(ccom_object)
+ccom_from_iot_object = decoder.iot_ticket(iot_message)
+# print(ccom_from_iot_object)
+ccom_json = json.dumps(encoder.ccom(ccom_from_iot_object), indent=4)
 print(ccom_json)
+iot_json = json.dumps(encoder.iot_ticket(ccom_from_iot_object[0]), indent=4)
+print(iot_json)
+iot_json = json.dumps(encoder.iot_ticket(ccom_from_iot_object[-1]), indent=4)
+print(iot_json)
