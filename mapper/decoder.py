581dc7eaf0203ca3b5a27e34b0075ff3baff0c4b
Jani Hietala <jani.hietala@vtt.fi>
2022-09-13 18:25:00 +0300

    Fix and clean decoders

--- mapper/decoder.py
+++ mapper/decoder.py
@@ -1,85 +1,58 @@
-from typing import Any
+from typing import Any, Iterable
 
 from mapper import types
 import utils
 
 
-def simple_decode_ccom(dct: dict[str, Any]) -> list[types.Entity] | None:
+def ccom(data: dict[str, Any]) -> list[types.Entity]:
 
     try:
-        entities: list[dict[str, Any]] = dct["CCOMData"]["entities"]
+        entities: list[dict[str, Any]] = data["CCOMData"]["entities"]
         return [
-            types.CCOM_TYPES[entity.get("@@type")].from_dict(entity)
-            for entity in entities
+            types.CCOM_TYPES[entity["@@type"]].from_dict(entity) for entity in entities
         ]
     except KeyError as error:
         raise ValueError("Invalid CCOM structure") from error
 
 
-def decode_ccom(dct: dict) -> types.Entity | dict:
-    """Decode CCOM dictionary object to CCOM types"""
+def iot_ticket_values(data: Iterable[dict]) -> list[types.SingleDataMeasurement]:
+    """Decode IoT-Ticket values to CCOM SingleDataMeasurements"""
 
-    if ccom_type_name := dct.get("@@type"):
-        return types.CCOM_TYPES[ccom_type_name].from_dict(dct)
-    elif "CCOMData" in dct:
-        return dct["CCOMData"]["entities"]
-    return dct
-
-
-def decode_iot(dct: dict) -> types.SingleDataMeasurement | dict:
-    """Decode IoT-Ticket dictionary object to CCOM types"""
-
-    if dct.keys() >= {"id", "v", "ts"}:
-        return types.SingleDataMeasurement.from_dict(
+    return [
+        types.SingleDataMeasurement.from_dict(
             {
+                "@@type": "SingleDataMeasurement",
                 "UUID": utils.new_uuid(),
                 "recorded": {
                     "@format": "RFC3339",
-                    "dateTime": utils.unix_to_date_string(dct["ts"]),
+                    "dateTime": utils.unix_to_utc_rfc3339(point["ts"]),
                 },
-                "data": {"measure": {"value": dct["v"]}},
-                "measurementLocation": {"UUID": dct["id"]},
+                "data": {"measure": {"value": point["v"]}},
             }
         )
-
-    return dct
-
-
-def iot_ticket_value(dct: dict) -> types.SingleDataMeasurement:
-    """Decode IoT-Ticket value to CCOM SingleDataMeasurement"""
-
-    timestamp = utils.unix_to_date_string(dct["ts"])
-    value = dct["v"]
-
-    return types.SingleDataMeasurement.from_dict(
-        {
-            "@@type": "SingleDataMeasurement",
-            "UUID": utils.new_uuid(),
-            "recorded": {
-                "@format": "RFC3339",
-                "dateTime": timestamp,
-            },
-            "data": {"measure": {"value": value}},
-        }
-    )
+        for point in data
+    ]
 
 
-def iot_ticket(dct: dict):
-    """Decode IoT-Ticket REST API response data to CCOM MeasurementLocation"""
+def iot_ticket(data: dict[str, Any]) -> list[types.MeasurementLocation]:
+    """Decode IoT-Ticket REST API response data to CCOM MeasurementLocations"""
 
+    nodes = data.get("datanodeReads", [])
     decoded = []
-    for node in dct.get("datanodeReads", []):
+    for node in nodes:
 
-        meas_loc = {
+        meas_loc: dict = {
             "@@type": "MeasurementLocation",
-            "UUID": "TODO",
+            "UUID": "TBD",
             "shortNames": [{"text": node["name"]}],
-            "measurements": [
-                iot_ticket_value(measurement) for measurement in node.get("values", [])
-            ],
+            "measurements": iot_ticket_values(node.get("values")),
         }
+
         if unit := node.get("unit"):
-            meas_loc["defaultUnitOfMeasure"] = {"UUID": "TODO", "name": unit}
+            meas_loc["defaultUnitOfMeasure"] = {
+                "UUID": "TBD",
+                "shortNames": [{"text": unit}],
+            }
 
         decoded.append(types.MeasurementLocation.from_dict(meas_loc))
 
