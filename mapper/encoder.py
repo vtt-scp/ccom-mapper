8feb5a615ffd4e0f7bf43bf460ec62393cde88cb
Jani Hietala <jani.hietala@vtt.fi>
2022-09-13 18:25:12 +0300

    Fix and clean the encoders

--- mapper/encoder.py
+++ mapper/encoder.py
@@ -1,35 +1,37 @@
-import json
+from typing import Iterable
 
 from mapper import types
 import utils
 
 
-def ccom(ccom_objects: types.Entity | list[types.Entity]) -> str:
-
-    if not isinstance(ccom_objects, list):
-        ccom_objects = [ccom_objects]
+def ccom(ccom_objects: Iterable[types.Entity]) -> dict:
+    """Encode CCOM entities to a CCOM message"""
 
     entities = [ccom_object.to_dict() for ccom_object in ccom_objects]
-    return json.dumps(
-        {"CCOMData": {"@ccomVersion": types.CCOM_VERSION, "entities": entities}}
-    )
+    return {"CCOMData": {"@ccomVersion": types.CCOM_VERSION, "entities": entities}}
 
 
 def iot_ticket(
-    ccom_objects: types.SingleDataMeasurement | list[types.SingleDataMeasurement],
-) -> str:
+    measurement_location: types.MeasurementLocation,
+) -> list[dict]:
+    """Encode CCOM MeasurementLocation to IoT-Ticket REST API message"""
+    print(measurement_location)
+    name = utils.require_attribute(measurement_location, "shortNames")[0].text
+    unit = None
+    if uom := getattr(measurement_location, "defaultUnitOfMeasure"):
+        unit = uom.shortNames[0].text
+    measurements: list = utils.require_attribute(measurement_location, "measurements")
 
-    if not isinstance(ccom_objects, list):
-        ccom_objects = [ccom_objects]
-
-    entities = [
-        {
-            "id": getattr(ccom_object.measurementLocation, "UUID"),
-            "ts": utils.date_string_to_unix(ccom_object.recorded.dateTime),
-            "v": getattr(
-                (ccom_object.data.measure or ccom_object.data.binaryObject), "value"
-            ),
+    entities = []
+    for measurement in measurements:
+        measure = measurement.data.measure or measurement.data.binaryObject
+        entity = {
+            "name": name,
+            "ts": utils.utc_rfc3339_to_unix(measurement.recorded.dateTime),
+            "v": measure.value,
         }
-        for ccom_object in ccom_objects
-    ]
-    return json.dumps(entities)
+        if unit:
+            entity["unit"] = unit
+        entities.append(entity)
+
+    return entities
